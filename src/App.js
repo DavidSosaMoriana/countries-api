b45a4c480d3cdd6c9b52f4c3e39607ff41463148
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-12 20:27:54 +0100

    :sparkles: search & regions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import Header from './components/Header';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <div className="app_body">
+                <div className="inputs">
+                    <div className="search_input">
+                        <SearchIcon />
+                        <input
+                            type="text"
+                            placeholder="Search for a country ..."
+                        />
+                    </div>
+                    <div className="select_region">
+                        <select>
+                            <option>All</option>
+                            <option>Africa</option>
+                            <option>Americas</option>
+                            <option>Asia</option>
+                            <option>Europe</option>
+                            <option>Oceania</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,14 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 export default function Header() {
     return (
-       <div className="header">
-        <div className="header_container">
-          <h2 className="logo">Where in the World?</h2>
-          <div className="switch_mode">
-            <DarkModeIcon />
-            <h3>Dark Mode</h3>
-          </div>
+        <div className="header">
+            <div className="header_container">
+                <h2 className="logo">Where in the World?</h2>
+                <div className="switch_mode">
+                    <DarkModeIcon />
+                    <h3>Dark Mode</h3>
+                </div>
+            </div>
         </div>
-
-       </div>
     );
 }
